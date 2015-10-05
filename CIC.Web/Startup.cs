d4b1d61c3469617f13b2d2f3369049ebcfd14129
DoojC <dujso.edin@gmail.com>
2015-10-05 20:43:10 +0200

    Automation System initial commit

---
+++ CIC.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CIC.Web.Startup))]
+namespace CIC.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
